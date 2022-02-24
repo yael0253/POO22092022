025979c886fbab59e9ad61af51e24f6d39738543
yael0253 <yaelztm03@gmail.com>
2022-02-24 12:40:39 -0600

    Iniciamos el archivo Foco.java

---
+++ clases/ConceptosBasicos/src/ico/fes/Foco.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes;
+
+/**
+ *
+ * @author Yael
+ */
+public class Foco {
+    private String color;
+    private String tipo;
+    private int tamano;
+
+    public Foco() {
+    }
+
+    public Foco(String color, String tipo, int tamano) {
+        this.color = color;
+        this.tipo = tipo;
+        this.tamano = tamano;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+
+}
