ac53ae971682e5a957c904c2bfbcc823bcb73959
yael0253 <yaelztm03@gmail.com>
2022-04-25 16:54:52 -0500

    Tarea Cuentahabiente

--- tareas/TareaCuentahabiente/src/tareacuentahabiente/PruebaEjemplo.java
+++ tareas/TareaCuentahabiente/src/tareacuentahabiente/PruebaEjemplo.java
@@ -12,10 +12,10 @@ public class PruebaEjemplo {
      public static void main(String[] args) {
     System.out.println("Arreglos de cuentahabientes");
         Cuentahabiente[] lista = new Cuentahabiente[5];
-        lista[0] = new Cuentahabiente("00001", "Martinez Isidro", 80000f);
+        lista[0] = new Cuentahabiente("00001", "Martinez Isidro", 85000f);
         lista[1] = new Cuentahabiente("00011", "Rosales Maria", 60000f);
         lista[2] = new Cuentahabiente("00111", "Luna Sergio", 90000f);
-        lista[3] = new Cuentahabiente("01111", "Rodiguez Ismael", 40000f);
+        lista[3] = new Cuentahabiente("01111", "Rodiguez Ismael", 49000f);
         lista[4] = new Cuentahabiente("11111", "Perez Juan", 30000f);
          for (int i = 0; i < lista.length; i++) {
              Cuentahabiente cuentahabiente = lista[i];
