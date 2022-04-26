b1bb38be4129220784a40526a2a8a29979efb05e
yael0253 <yaelztm03@gmail.com>
2022-04-25 19:13:52 -0500

    Tarea cuentahabiente

--- tareas/TareaCuentahabiente/src/tareacuentahabiente/PruebaEjemplo.java
+++ tareas/TareaCuentahabiente/src/tareacuentahabiente/PruebaEjemplo.java
@@ -9,18 +9,24 @@ package tareacuentahabiente;
  * @author Yael
  */
 public class PruebaEjemplo {
-     public static void main(String[] args) {
-    System.out.println("Arreglos de cuentahabientes");
+
+    public static void main(String[] args) {
+        System.out.println("Arreglos de cuentahabientes");
         Cuentahabiente[] lista = new Cuentahabiente[5];
         lista[0] = new Cuentahabiente("00001", "Martinez Isidro", 85000f);
         lista[1] = new Cuentahabiente("00011", "Rosales Maria", 60000f);
         lista[2] = new Cuentahabiente("00111", "Luna Sergio", 90000f);
         lista[3] = new Cuentahabiente("01111", "Rodiguez Ismael", 49000f);
         lista[4] = new Cuentahabiente("11111", "Perez Juan", 30000f);
-         for (int i = 0; i < lista.length; i++) {
-             Cuentahabiente cuentahabiente = lista[i];
-             System.out.println(cuentahabiente.evaluarNivelCliente());
-         }
+        for (int i = 0; i < lista.length; i++) {
+            Cuentahabiente cuentahabiente = lista[i];
+            System.out.println(cuentahabiente.evaluarNivelCliente());
+        }
+        System.out.println("Arreglo con For Each");
+        for (Cuentahabiente cuentahabiente : lista) {
+            System.out.println(cuentahabiente.evaluarNivelCliente());
+
+        }
     }
 
 }
